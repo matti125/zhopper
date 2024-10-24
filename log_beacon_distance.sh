87c7183c838a9655509dea5b8b3b2f39bd05999e
mazas <mazasgame@gmail.com>
2024-10-24 17:24:36 +0100

    changed to append to log file instead of clearing

--- log_beacon_distance.sh
+++ log_beacon_distance.sh
@@ -5,8 +5,8 @@ HOST=${HOST:-"ratos2.local"}     # Default value: ratos2.local
 INTERVAL=${INTERVAL:-5}          # Default interval: 5 seconds
 BEACON_LOG=${BEACON_LOG:-"./out/beacon_query.log"}  # Default log file
 MEASUREMENT=${MEASUREMENT:-"gantry"}          # Default measurement name
-BUCKET=${BUCKET:-"r3"}           # Default InfluxDB bucket
-TAG=${TAG:-"case=stock,flow_direction=stock"}           # Default InfluxDB bucket
+BUCKET=${BUCKET:-"gantry"}           # Default InfluxDB bucket
+TAG=${TAG:-"printer=unknown,case=unknown"}           # Default InfluxDB tags
 
 # Start the loop to send the beacon query and process the responses
 while true; do
@@ -14,6 +14,6 @@ while true; do
     sleep "$INTERVAL"      # Wait for the specified interval before repeating
 done | ./gcode_response_spy.py --host "$HOST" | \
     ./convert_beacon_query_to_influx.py --result-header "// Last reading:" --measurement "$MEASUREMENT" --tag "$TAG" | \
-    tee "$BEACON_LOG" | \
+    tee -a "$BEACON_LOG" | \
     ./influx_write_by_line.py --bucket "$BUCKET"
 cat
